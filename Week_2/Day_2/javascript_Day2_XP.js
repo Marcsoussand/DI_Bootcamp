bd7997d480e5a07b11804e8f9f62f80779789f36
Marcsoussand <marc.soussand@gmail.com>
2021-01-18 22:24:56 +0200

    <Week_2_Day2>

--- Week_2/Day_2/javascript_Day2_XP.js
+++ Week_2/Day_2/javascript_Day2_XP.js
@@ -15,4 +15,31 @@ console.log(newDog.toUpperCase());
 console.log(newDog.toLowerCase());
 if (newDog == "Chihuahua") {
 	console.log("I love Chihuahua, it’s my favorite dog breed");
+} else {
+	console.log("I don't care, I prefer cats");
 }
+
+// Exercise 3
+
+let number3 = prompt("Please give me a number : ");
+let ratio = number3%2;
+if (ratio == 0) {
+	console.log(number3 + " is an even number");
+} else {
+	console.log(number3 + " is not an even number");
+}
+
+// Exercise 4
+
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+let lg=users.length-2;
+if (users.length == 0) {
+	console.log("No one is online");
+} else if (users.length == 1) {
+	console.log(users[0] + " is online");
+} else if (users.length == 2) {
+	console.log(users[0] + " and " + users[1]+ " are online.")
+} else {
+	console.log(users[0] + ", " +users[1]+" and "+lg+ " more are online");
+}
+
