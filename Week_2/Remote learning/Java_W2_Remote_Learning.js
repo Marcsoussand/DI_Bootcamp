58253e3d6ec9fa6a9e36a3cfa062579e20dbf2d9
Marcsoussand <marc.soussand@gmail.com>
2021-01-22 00:18:34 +0200

    <Week2_Day5>

---
+++ Week_2/Remote learning/Java_W2_Remote_Learning.js
@@ -0,0 +1,37 @@
+// Exercise 1
+// output 10
+var num = 8;
+var num = 10;
+console.log(num);
+
+// Exercise 2
+// Change the code so the var i will be undefined outside of the for loop
+// Think I am missing something, seems strange to do it this way
+
+function numbers() {
+    
+    
+    for (var i = 0; i < 5; i += 1) {
+      console.log(i);
+    }
+    i=undefined;
+      console.log(i);
+  }
+  numbers();
+
+//Exercise 3
+ let globalAmount = 10000;
+ var vAT = 0.2;
+ var expensesRevenue = [150,-250,600,-1000,3000,-250];
+
+ for (i=0;i<expensesRevenue.length;i++){
+        if(expensesRevenue[i]<0) {
+            expensesRevenue[i] *=(1+vAT);
+        } 
+ }
+console.log("My expenses with VAT and Revenue : "+expensesRevenue);
+for (i=0;i<expensesRevenue.length;i++) {
+    globalAmount +=expensesRevenue[i];
+}
+
+console.log("At the end, I will have : " +globalAmount);
