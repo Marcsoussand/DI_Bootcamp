6442b1a8ee3423fa7211c19374d6925a223e7341
Marcsoussand <marc.soussand@gmail.com>
2021-01-21 02:45:43 +0200

    <Week2_Day4>

---
+++ Week_2/Day_4/Daily_Challenge/Java_W2_D4_Daily_Challenge.js
@@ -0,0 +1,30 @@
+let entry = prompt("Please enter multiple words separated by commas :");
+let array = entry.split(", ");
+// console.log(array);
+let lengthMax=0;
+let lengthVar=0;
+
+for (i=0;i<array.length-1;i++) {
+    lengthVar=array[i].length;
+    // console.log("var"+lengthVar);
+    // console.log("max"+lengthMax);
+    if (lengthVar>lengthMax) {
+        lengthMax = lengthVar;
+    } 
+}
+
+// console.log(lengthMax);
+let stars = "";
+let print = " ";
+
+for (i=0;i<lengthMax+4;i++) {
+    stars +="*"
+}
+console.log(stars);
+for (i=0;i<array.length;i++){
+    lengthVar=array[i].length;
+    var till = lengthMax-lengthVar;
+    console.log("* "+array[i]+print.repeat(till)+" *");
+}
+console.log(stars);
+
