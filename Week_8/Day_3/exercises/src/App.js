c2084975361e688ce212e697bbfbc1d013ac63bf
Marcsoussand <marc.soussand@gmail.com>
2021-03-03 02:19:07 +0200

    <Week8_Day3>

--- Week_8/Day_3/exercises/src/App.js
+++ Week_8/Day_3/exercises/src/App.js
@@ -4,36 +4,123 @@ import './App.css';
 
 class App extends React.Component {
   //Exercise XP #1 - React Lifecycle
-  constructor(){
+  //   constructor(){
+  //     super();
+  //     this.state = {
+  //       color:'red',
+  //     }
+  //   }
+  // changeBlue = () =>{
+  //   this.setState({color:'blue'})
+  // }
+
+  // shouldComponentUpdate(){
+  //   return false;
+  // }
+
+  //   componentDidMount() {
+  // const timer = setTimeout(() => {
+  //   this.setState({color:'yellow'});
+  // }, 5000);
+  //   }
+
+  //   render(){
+
+  // const {color} = this.state;
+  // return (
+  //   <>
+  //     <h1>My favourite colour is {color}</h1>
+  //     <button onClick={this.changeBlue}>Change color to blue</button>
+  //     </>
+  // )
+  //   }
+
+  //Exercise XP #2 - React Lifecycle 
+  //I did not succeed to stop the update things, is not working
+  //Error: Maximum update depth exceeded
+  //   constructor() {
+  //     super();
+  //     this.state = {
+  //       color: 'red',
+  //       addText: '',
+  //     }
+  //   }
+  //   changeBlue = () => {
+  //     this.setState({ color: 'blue' })
+  //   }
+
+  //   componentDidUpdate(prevProps, prevState) {
+  //     if (prevProps.color !== this.state.color) {
+  //       return this.setState({ addText: 'The updated favorite is yellow' });
+  //     }
+  //   }
+  //   // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   //   if (prevProps.addText == '') {
+  //   //   return this.setState({ addText: 'Before the update, the favorite was red' });
+  //   //   }
+  //   // }
+
+  //   componentDidMount() {
+  //     const timer = setTimeout(() => {
+  //       this.setState({ color: 'yellow' });
+  //     }, 5000);
+  //   }
+
+  //   render() {
+
+  //     const { color, addText } = this.state;
+  //     return (
+  //       <>
+  //         <h1>My favourite colour is {color}</h1>
+  //         <button onClick={this.changeBlue}>Change color to blue</button>
+  //         <div>{addText}</div>
+  //       </>
+  //     )
+  //   }
+
+
+  //Exercise XP #3 - React Events
+
+  constructor() {
     super();
     this.state = {
-      color:'red',
+      // shot: "",
     }
   }
-changeBlue = () =>{
-  this.setState({color:'blue'})
-}
 
-shouldComponentUpdate(){
-  return false;
-}
+    greatShot = () => {
+      
+      alert('Great Shot !')
+    }
 
-  componentDidMount() {
-const timer = setTimeout(() => {
-  this.setState({color:'yellow'});
-}, 5000);
+    shoot = ()=> {
+      alert('Nice Shot!')
+    }
+
+    shoot2 = ()=> {
+      alert(this)
+    }
+
+
+    componentDidMount() {
+    }
+
+    render(){
+      // const { shot } = this.state;
+
+      return (
+
+        <>
+          <div>Exercise 1</div>
+          <button onClick={this.greatShot}>Take the shot</button>
+          <div>Exercise 2</div>
+          <button onClick={this.shoot}>Shoot Again</button>
+          <div>Exercise 3</div>
+          <button onClick={this.shoot2}>Keep shooting</button>
+          
+        </>
+      )
+    }
+
   }
-
-  render(){
-
-const {color} = this.state;
-return (
-  <>
-    <h1>My favourite colour is {color}</h1>
-    <button onClick={this.changeBlue}>Change color to blue</button>
-    </>
-)
-  }
-}
-
-export default App;
+  export default App;
